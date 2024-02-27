16f0544397d1a72c5ed77102e02fc2a2222847f9
n-dett <124851780+n-dett@users.noreply.github.com>
2024-02-26 22:58:03 -0600

    Edit heading styles

---
+++ daylight.js
@@ -0,0 +1,17 @@
+
+///// FOR TODAY'S DAYLIGHT
+
+// let todayDate = today;
+// let currentTime = currentTime
+
+
+///// FOR NEXT DAYLIGHT ////
+
+// let tomorrowDate = tomorrowDate
+// let tomorrowTime = tomorrowTime
+
+
+let endDaylightTime = "18:37:00";
+
+
+console.log(todayDate);
