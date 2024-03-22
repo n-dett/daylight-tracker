8a551b81f5ba4f80a9639f1840bbb8f3f874a0c1
n-dett <124851780+n-dett@users.noreply.github.com>
2024-03-21 22:58:37 -0500

    Store user input

--- daylight.js
+++ daylight.js
@@ -11,7 +11,28 @@
 // let tomorrowTime = tomorrowTime
 
 
-let endDaylightTime = "18:37:00";
+// Get user zip code
+
+let userInput = document.getElementById("zip-input");
+
+document.getElementById("enter-button").addEventListener("click", function(){
+    userInput.value;
+    console.log(userInput.value)
+})
+
+userInput.addEventListener("keypress", function(e){
+    if(e.key === "Enter"){
+        e.preventDefault();
+        document.getElementById("enter-button").click();
+    }
+})
 
 
-console.log(todayDate);
+
+
+
+
+
+
+//let endDaylightTime = "18:37:00";
+
