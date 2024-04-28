03d3bece4e779a0d919f3b3ee80f9d880eb66c44
n-dett <124851780+n-dett@users.noreply.github.com>
2024-04-27 20:59:18 -0500

    Add dark theme

--- daylight.js
+++ daylight.js
@@ -11,6 +11,9 @@
 // let tomorrowTime = tomorrowTime
 
 
+
+
+
 // Get user zip code
 
 let userInput = document.getElementById("zip-input");
@@ -22,10 +25,38 @@ document.getElementById("enter-button").addEventListener("click", function(){
 
 userInput.addEventListener("keypress", function(e){
     if(e.key === "Enter"){
-        e.preventDefault();
         document.getElementById("enter-button").click();
     }
 })
+
+
+
+
+
+// Change colors at sunset and sunrise
+function changeColorTheme(){
+    // Check current time
+    const currentTime = new Date();
+
+    let daylightTimes = [
+        {time: "6:00:00 PM", cssClass: "sunsetColors"},
+        {time: "7:00:00 PM", cssClass: "lastLightColors"},
+        {time: "6:00:00 AM", cssClass: "firstLightColors"},
+        {time: "7:00:00 AM", cssClass: "sunriseColors"},
+    ]
+
+    // Set sunset time
+    let sunsetTimeString = "6:00:00 PM";
+    let sunsetTimeArray = sunsetTimeString.split(/[:\s]/);
+    let sunsetHour = sunsetTimeArray[0];
+    let sunsetMinutes = sunsetTimeArray[1];
+
+
+// foreach loop to iterate over each time and compare to current time
+
+}
+// Check time every minute
+//setInterval(changeColorTheme, 60000);
 
 
 
